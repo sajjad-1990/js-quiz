ea6f8663195862b4183cda19d170a3a868f0face
sajjad-1990 <sajjad.m.rezaei@gmail.com>
2021-07-23 22:48:55 -0700

    first commit

---
+++ questions.js
@@ -0,0 +1,178 @@
+// var questions = [
+//     {
+
+//         "question": "پایتخت ایران کدام شهر است؟",
+//         "option1": "مشهد",
+//         "option2": "کرمان",
+//         "option3": "تهران",
+//         "option4": "قم",
+//         "answer": 3
+//     },
+//     {
+        
+//         "question": "انقلاب اسلامی ایران در چه سالی رخ داد؟",
+//         "option1": "1360",
+//         "option2": "1350",
+//         "option3": "1345",
+//         "option4": "1357",
+//         "answer": 4
+
+//     },
+//     {
+//         "question": "ستارخان در کدام شهر به دنیا آمده؟",
+//         "option1": "تبریز",
+//         "option2": "تهران",
+//         "option3": "مشهد",
+//         "option4": "بندر عباس",
+//         "answer": 1
+//     },
+//     {
+//         "question": "قله توچال در کدام استان می باشد؟",
+//         "option1": "تبریز",
+//         "option2": "تهران",
+//         "option3": "مشهد",
+//         "option4": "بندر عباس",
+//         "answer": 2
+//     },
+//     {
+//         "question": "کدام یک از گزینه های زیر برای طراحی صفحات سایت می باشد؟",
+//         "option1": "php",
+//         "option2": "java",
+//         "option3": "c#",
+//         "option4": "css",
+//         "answer": 4
+//     },
+//     {
+//         "question": "با کدام نوع تعریف متغییر ها دیگر نمیتوان محتوای آن را تغییر داد؟",
+//         "option1": "let",
+//         "option2": "var",
+//         "option3": "const",
+//         "option4": "int",
+//         "answer": 3
+//     },
+//     {
+//         "question": "سوهان مربوط به سوقات کدام شهر است؟",
+//         "option1": "قم",
+//         "option2": "مشهد",
+//         "option3": "شیراز",
+//         "option4": "تبریز",
+//         "answer": 1
+//     }
+//     ,
+//     {
+//         "question": "گز سوقات کدام شهر است؟",
+//         "option1": "کرج",
+//         "option2": "کردستان",
+//         "option3": "لرستان",
+//         "option4": "اصفهان",
+//         "answer": 4
+//     },
+//     {
+//         "question": "استقلال چند بار قهرمان آسیا شده؟",
+//         "option1": "2",
+//         "option2": "1",
+//         "option3": "0",
+//         "option4": "5",
+//         "answer": 1
+//     },
+//     {
+//         "question": "سال 99 چه تیمی قهرمان لیگ شد؟",
+//         "option1": "استقلال",
+//         "option2": "تراکتور",
+//         "option3": "پرسپولیس",
+//         "option4": "پدیده",
+//         "answer": 3
+//     }
+// ];
+
+
+var questions = [
+    {
+        "question": "Web pages are designed using?",
+        "option1" : "HTML",
+        "option2": "CSS",
+        "option3": "Javascript",
+        "option4": "Jquery",
+        "answer": 2
+    }, 
+    {
+        "question": "Web pages are designed using?",
+        "option1" : "HTML",
+        "option2": "CSS",
+        "option3": "Javascript",
+        "option4": "Jquery",
+        "answer": 2
+    },
+    {
+        "question": "____ makes websites dynamic",
+        "option1" : "CSS",
+        "option2": "HTML",
+        "option3": "Javascript",
+        "option4": "Bootstrap",
+        "answer": 3
+    },
+    {
+        "question": "What is HTML used for?",
+        "option1" : "Skeleton of website",
+        "option2": "Design a website",
+        "option3": "Make website dynamic",
+        "option4": "Database of a website",
+        "answer": 1
+    },
+    {
+        "question": "Which CSS property changes background color?", 
+        "option1": "colour",
+        "option2": "bgColor",
+        "option3": "color",
+        "option4": "background-color",
+        "answer": 4
+    }, 
+    {
+        "question": "Which selector selects all the elements in CSS?", 
+        "option1": "*",
+        "option2": "+",
+        "option3": "$",
+        "option4": "#",
+        "answer": 1
+    }, 
+    {
+        "question": "What is the value of a in a+=b where a=10 & b=5?", 
+        "option1": "10",
+        "option2": "5",
+        "option3": "15",
+        "option4": "20",
+        "answer": 3
+    }, 
+    {
+        "question": "What does 'document' denote in a javascript statement?", 
+        "option1": "Javascript file",
+        "option2": "Css file",
+        "option3": "HTML page",
+        "option4": "XML document",
+        "answer": 3
+    }, 
+    {
+        "question": "Which function is used to calculate an expression?", 
+        "option1": "calculate()",
+        "option2": "calc()",
+        "option3": "evaluate()",
+        "option4": "eval()",
+        "answer": 4
+    }, 
+    {
+        "question": "How do you call a function? ", 
+        "option1": "call.function_name",
+        "option2": "function_name.call",
+        "option3": "call(function_name)",
+        "option4": "function_name()",
+        "answer": 4
+    }, 
+    {
+        "question": "Javascript statements should end with a ... ", 
+        "option1": "Comma",
+        "option2": "Semicolon",
+        "option3": "Period",
+        "option4": "Exclamation",
+        "answer": 2
+    }		
+    ];
